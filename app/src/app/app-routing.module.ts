220e5309e2b8aaafe1732f7da19ec22ab8947db7
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2019-03-08 16:09:41 +0100

    Added base routing and authentication code.

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TestComponent } from './test/test.component';
+import { AuthGuard } from './core/guards/auth.guard';
+
+// Layouts
+import { DefaultLayoutComponent } from './core/layouts/default/default.component';
+import { LoginLayoutComponent } from './core/layouts/login/login.component';
+
+
+// Core-pages
+import { LoginComponent } from './core/pages/login/login.component';
+import { UnauthorizedComponent } from './core/pages/unauthorized/unauthorized.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', component: TestComponent, pathMatch: 'full'},
+      { path: 'test', component: TestComponent, pathMatch: 'full'},
+    ]
+  },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      { path: 'unauthorized', component: UnauthorizedComponent, pathMatch: 'full'},
+      { path: 'login', component: LoginComponent, pathMatch: 'full'},
+    ]
+  },
+  { path: '**', component: TestComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
+
+// ngrx apparently removed payload from the Action interface since they tought it was optional
+// This is against flux patterns which dictates that a Action consists of a type and a payload.
+// For flexibility purpouses we will add payload as an optional instead.
+// We are patching for TS linting sake.
+declare module '@ngrx/store' {
+  interface Action {
+    type: string;
+    payload?: any;
+  }
+}
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { reducers, metaReducers } from './app-reducer';
+import { environment } from '../environments/environment';
+
+import { CoreModule } from './core/core.module';
+
+/* Effects */
+import { AuthEffects } from './data/auth/auth.effects';
+
+/* Services */
+import { AuthService } from './data/auth/auth.service';
+
+/* Components */
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    AppRoutingModule,
+
+
+    HttpClientModule,
+    EffectsModule.forRoot([AuthEffects]),
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
