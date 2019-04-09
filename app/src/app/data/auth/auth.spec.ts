c148050e5b023556cfd6b8141212c0ba8b2d9c17
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2019-04-09 17:11:25 +0200

    New Tests: Written tests for auth reducer and actions.

---
+++ app/src/app/data/auth/auth.spec.ts
@@ -0,0 +1,47 @@
+import { AuthReducer, AuthActions, AuthSelectors } from '.';
+
+describe('AuthReducer', () => {
+  describe('undefined action', () => {
+    it('should return the default state', () => {
+      const { initialState } = AuthReducer;
+      const action = { type: undefined, payload: undefined };
+      const state = AuthReducer.authReducer(undefined, action);
+
+      expect(state).toBe(initialState);
+    });
+  });
+
+  const loginPayload = {
+    jwtToken: {
+      raw: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJ1c20vYXV0aGVudGljYXRpb24iLCJpc3MiOiJ1c20iLCJzdWIiOiJhdXRoZW50aWNhdGlvbiIsImlhdCI6MTU1NDgxOTQxMywiZXhwIjoxNTU0ODIzMzAyLCJ1c2VyTmFtZSI6InZtc191c2VyIn0.6lLd-GQOtz4VhkAYqWeCLr7_OaMXI4F9JpSj6eaeCNU',
+      decoded: {
+        jti: 'usm/authentication',
+        iss: 'usm',
+        sub: 'authentication',
+        iat: 1554819413,
+        exp: 1554821213,
+        userName: 'vms_user'
+      }
+    },
+    data: {
+      username: 'vms_user'
+    }
+  };
+
+  describe('ActionTypes.LoginSuccess', () => {
+    it('should return a acceptable user object.', () => {
+      const state = AuthReducer.authReducer(undefined, new AuthActions.LoginSuccess(loginPayload));
+
+      expect(state).toEqual({ user: loginPayload });
+    });
+  });
+
+  describe('ActionTypes.LoginFailed', () => {
+    it('should return a acceptable user object.', () => {
+      const { initialState } = AuthReducer;
+      const state = AuthReducer.authReducer(undefined, new AuthActions.LoginFailed({ error: 'failed' }));
+
+      expect(state).toEqual(initialState);
+    });
+  });
+});
