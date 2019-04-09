713d51d5311ffcd4f01c8df414d338f672a84b90
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2019-04-09 11:22:39 +0200

    Cleaned up LoginComponent spec.

--- app/src/app/core/pages/login/login.component.spec.ts
+++ app/src/app/core/pages/login/login.component.spec.ts
@@ -1,5 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 
@@ -11,11 +10,7 @@ import { LoginComponent } from './login.component';
 import { AuthActions } from '../../../data/auth';
 
 // For MDB Angular Free
-import {
-  NavbarModule, WavesModule, ButtonsModule, CheckboxModule,
-  InputsModule, IconsModule
-} from 'angular-bootstrap-md';
-
+import { CheckboxModule } from 'angular-bootstrap-md';
 
 describe('LoginComponent', () => {
 
@@ -26,15 +21,9 @@ describe('LoginComponent', () => {
       declarations: [ LoginComponent ],
       imports: [
         TestingModule,
-        // RouterTestingModule.withRoutes([
-        //   { path: 'map/realtime' , component: LoginComponent }
-        // ]),
-        /* MDB Imports: */
-        NavbarModule, WavesModule, ButtonsModule, CheckboxModule,
-        InputsModule, IconsModule
+        CheckboxModule
       ],
       providers: [
-        // { provide: RouterTestingModule, useValue: mockRouter }
         { provide: Router, useValue: mockRouter }
       ]
     })
@@ -56,13 +45,11 @@ describe('LoginComponent', () => {
   it('should dispatch AuthActions.Login on submit and redirect', () => {
     const { fixture, component } = setup();
     const store = TestBed.get(Store);
-    // console.warn(component.store);
     const dispatchSpy = spyOn(store, 'dispatch');
 
     const usernameElement = fixture.debugElement.query(By.css('input#loginFormUsername'));
     const passwordElement = fixture.debugElement.query(By.css('input#loginFormPassword'));
     const submitElement = fixture.debugElement.query(By.css('button'));
-    // AuthActions.Login
 
     const username = "Username123";
     const password = "Password123";
