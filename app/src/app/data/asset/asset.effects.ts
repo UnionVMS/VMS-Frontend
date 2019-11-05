25bbbe946ac06ca48f08e5b5cd4debfde25a7a02
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2019-11-05 13:50:49 +0100

    Updated api-query for getting mobile terminals to new version.

--- app/src/app/data/asset/asset.effects.ts
+++ app/src/app/data/asset/asset.effects.ts
@@ -334,12 +334,7 @@ export class AssetEffects {
             return [
               AssetActions.setFullAsset({ asset }),
               MobileTerminalActions.search({
-                query: {
-                  pagination: { page: 1, listSize: 1000000 },
-                  mobileTerminalSearchCriteria: {
-                    criterias: asset.mobileTerminalIds.map(mobileTerminalId => ({ key: 'MOBILETERMINAL_ID', value: mobileTerminalId }))
-                  }
-                },
+                query: { mobileterminalIds: asset.mobileTerminalIds },
                 includeArchived: false,
               })
             ];
