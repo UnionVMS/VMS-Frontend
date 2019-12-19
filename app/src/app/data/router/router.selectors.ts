da5e5a1fd5ae93a4199d8f5945423ed94fa5caa5
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2019-12-19 16:41:11 +0100

    Fixed tests and linting rules.

--- app/src/app/data/router/router.selectors.ts
+++ app/src/app/data/router/router.selectors.ts
@@ -2,8 +2,10 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ActivatedRoute } from '@angular/router';
 import { MergedRouteReducerState, MergedRoute } from './router.interfaces';
 import { routerStateConfig } from '@modules/router/ngrx-router.module';
+import { State } from '@app/app-reducer';
 
-export const getRouterReducerState = createFeatureSelector<MergedRouteReducerState>(routerStateConfig.stateKey);
+// export const getRouterReducerState = createFeatureSelector<MergedRouteReducerState>(routerStateConfig.stateKey);
+export const getRouterReducerState = (state: State) => state[routerStateConfig.stateKey];
 export const getMergedRoute = createSelector(getRouterReducerState, (routerReducerState) => {
   return typeof routerReducerState !== 'undefined' ? routerReducerState.state : {} as MergedRoute;
 });
