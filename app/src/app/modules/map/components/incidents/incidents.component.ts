1edea84bd38ead096ef6805ee11f25eafab44f6b
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2020-01-08 11:13:07 +0100

    New Feature: 'Incident not sending' switch button for resolved is now implemented.

--- app/src/app/modules/map/components/incidents/incidents.component.ts
+++ app/src/app/modules/map/components/incidents/incidents.component.ts
@@ -12,11 +12,21 @@ export class IncidentsComponent {
   @Input() incidentNotifications: IncidentInterfaces.incidentNotificationsCollections;
   @Input() selectIncident: (incident: IncidentInterfaces.assetNotSendingIncident) => void;
 
+  public resolved = false;
+
   public incidentStatusClass = {
     MANUAL_POSITION_MODE: 'dangerLvl1',
     ATTEMPTED_CONTACT: 'dangerLvl5',
     RESOLVED: 'dangerLvl0',
   };
+
+  public trackByIncidents = (index: number, item: IncidentInterfaces.assetNotSendingIncident) => {
+    return item.assetId + '-' + (this.resolved ? 'r' : 'nr');
+  }
+
+  public switchShowResolved = () => {
+    this.resolved = !this.resolved;
+  }
 
   formatDate(incident: IncidentInterfaces.assetNotSendingIncident) {
     const date = new Date((incident.lastKnownLocation.timestamp as number) * 1000);
