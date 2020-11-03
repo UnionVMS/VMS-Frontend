f3b39005d3239e79837103ab8e7566cade9a6d64
Daniel Wirdehäll <daniel.wirdehall@havochvatten.se>
2020-11-03 17:07:51 +0100

    New Feature: Notifications now have animations and better positioning of end-block (close-button and countdown). New Feature: User setting for experimentalFeatures can now be enabled in settings view.

--- app/src/app/core/components/notifications/notifications.component.ts
+++ app/src/app/core/components/notifications/notifications.component.ts
@@ -1,16 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { NotificationsTypes } from '@data/notifications';
+
+const animationTimeMs = 300;
 
 @Component({
   selector: 'core-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('dropAndFadeAnimation', [
+      // the "in" style determines the "resting" state of the element when it is visible.
+      state('in', style({opacity: 1, 'margin-top': '0px' })),
+
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({opacity: 0, 'margin-top': '-40px'}),
+        animate(animationTimeMs)
+      ]),
+
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate(animationTimeMs, style({opacity: 0, 'margin-top': '-40px'}))
+      )
+    ]),
+  ]
 })
 
-export class NotificationsComponent {
+export class NotificationsComponent implements OnChanges {
   @Input() notifications: NotificationsTypes.State;
   @Input() dismiss: (type: string, id: string) => void;
   @Input() overlay: boolean;
+
+  public notificationsVisible = false;
+  public lastTimeoutId: number | undefined;
+
+  ngOnChanges() {
+    if(
+      this.notifications.success.length === 0 &&
+      this.notifications.notices.length === 0 &&
+      this.notifications.errors.length === 0
+    ) {
+      // We have to wait for the animation to finish before we remove the wrapping notification container.
+      this.lastTimeoutId = window.setTimeout(() => {
+        this.notificationsVisible = false;
+      }, animationTimeMs);
+    } else {
+      window.clearTimeout(this.lastTimeoutId);
+      this.notificationsVisible = true;
+    }
+  }
 
   addConditionalClasses() {
     return this.overlay === true ? 'overlay' : '';
--- app/src/app/data/user-settings/user-settings.reducer.ts
+++ app/src/app/data/user-settings/user-settings.reducer.ts
@@ -3,12 +3,17 @@ import * as UserSettingsActions from './user-settings.actions';
 import * as Types from './user-settings.types';
 
 export const initialState: Types.State = {
-  timezone: 'Etc/UTC'
+  timezone: 'UTC',
+  experimentalFeaturesEnabled: false,
 };
 
 export const userSettingsReducer = createReducer(initialState,
   on(UserSettingsActions.setTimezone, (state, { timezone }) => ({
     ...state,
     timezone
+  })),
+  on(UserSettingsActions.setExperimentalFeaturesEnabled, (state, { experimentalFeaturesEnabled }) => ({
+    ...state,
+    experimentalFeaturesEnabled
   })),
 );
