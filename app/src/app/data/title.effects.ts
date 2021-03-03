c58a8eb5b5a645fe3c1e59baccdbceade869019f
johan <johan.soderblom@havochvatten.se>
2021-03-03 18:09:31 +0100

    remove comma

--- app/src/app/data/title.effects.ts
+++ app/src/app/data/title.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions,  createEffect, ofType } from '@ngrx/effects';
-import { tap, } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { Title } from '@angular/platform-browser';
 import { routerNavigatedAction } from '@ngrx/router-store';
  
