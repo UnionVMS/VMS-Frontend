87c78a2aa0204818283239f6ed88a91dab43bced
johan <johan.soderblom@havochvatten.se>
2021-03-04 08:56:16 +0100

    PR comment fix

--- app/src/app/data/title.effects.ts
+++ app/src/app/data/title.effects.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Actions,  createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { tap } from 'rxjs/operators';
 import { Title } from '@angular/platform-browser';
 import { routerNavigatedAction } from '@ngrx/router-store';
  
 @Injectable()
 export class TitleEffects {
-    constructor(
-      private readonly actions$: Actions,
-      private titleService: Title
-    ) {}
+  constructor(
+    private readonly actions$: Actions,
+    private titleService: Title
+  ) {}
 
-    updateTitle$ = createEffect(() =>
+  updateTitle$ = createEffect(() =>
     this.actions$.pipe(
       ofType(routerNavigatedAction),
-      tap((store) => {
-        let titleText = store.payload.routerState.url.replaceAll('/', ' ');
-        let textReplace = Object.values(store.payload.routerState.params).join('');
-        this.titleService.setTitle(titleText.replace(textReplace, '') + ' UVMS')})
+      tap((action) => {
+        let titleText = action.payload.routerState.url.replaceAll('/', ' ');
+        let textReplace = Object.values(action.payload.routerState.params).join('');
+        this.titleService.setTitle(titleText.replace(textReplace, '') + ' UVMS')
+      })
     ),
     {
       dispatch: false,
-    });   
+    }
+  );   
 }
