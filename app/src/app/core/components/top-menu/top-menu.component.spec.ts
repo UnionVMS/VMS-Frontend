6484e5ae9af6e29b6d26045c814ed3c3f8fd6924
johan <johan.soderblom@havochvatten.se>
2021-05-25 14:44:28 +0200

    fix top menu test

--- app/src/app/core/components/top-menu/top-menu.component.spec.ts
+++ app/src/app/core/components/top-menu/top-menu.component.spec.ts
@@ -55,10 +55,10 @@ describe('TopMenuComponent', () => {
     const links = layoutElement.querySelectorAll('.navbar a');
     // expect(links[0].textContent).toBe('F-Admin');
     expect(links[1].textContent).toContain('Asset');
-    expect(links[3].textContent).toContain('Realtime map');
-    expect(links[4].textContent).toContain('Reports map');
-    expect(links[5].textContent).toContain('My Settings');
-    expect(links[6].textContent).toContain('Logout');
+    expect(links[2].textContent).toContain('Realtime map');
+    expect(links[3].textContent).toContain('Reports map');
+    expect(links[4].textContent).toContain('My Settings');
+    expect(links[5].textContent).toContain('Logout');
     //expect(links[7].textContent).toBe('ADMIN');
 
     // check so admin link don´t show when isAdmin = false
